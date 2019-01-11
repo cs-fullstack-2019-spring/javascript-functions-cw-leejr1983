076a81b2878743fb4883bdecc829053f1bf3419a
leejr1983 <leejr1983@yahoo.com>
2019-01-11 17:00:21 -0600

    classwork

---
+++ Problem1.js
@@ -0,0 +1,13 @@
+// Problem 1
+// Using the code below:
+// printNumbers();
+// Create a printNumbers function to print integers from -25 to 20.
+
+function printNumbers(){
+   for (var i = -25; i <= 20; i++){
+      console.log(i);
+
+   }
+}
+
+printNumbers();
---
+++ Problem2.js
@@ -0,0 +1,10 @@
+// Problem 2
+// Create a function that’s passed a firstName and lastName. Return lastName comma firstName.
+
+function name(firstName,lastName) {
+var whatever= lastName + ", " + firstName
+    return whatever
+
+}
+
+console.log (name("Leighton","Wright"))
