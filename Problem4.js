6fbdb68b7b0ff36e30c2777eb02bef018dd18cbf
leejr1983 <leejr1983@yahoo.com>
2019-01-12 21:16:47 -0600

    classwork

---
+++ Problem4.js
@@ -0,0 +1,17 @@
+// Problem 4
+// Create a function called checkPassword. Send two string variables to the checkPassword function to check if the strings are equal.
+// Return true if they are equal and false if they are not equal. Print the function's return value.
+
+password = prompt ("Please enter password")
+password2 = prompt ("please enter 2nd password")
+
+function checkPassword() {
+
+    if (password == password2)
+        return true;
+
+    else return false;
+}
+
+
+console.log (checkPassword())
