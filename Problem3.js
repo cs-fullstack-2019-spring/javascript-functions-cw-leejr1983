61fbf8549d4985f77f98e9807d98cdad4c89c047
leejr1983 <leejr1983@yahoo.com>
2019-01-12 20:48:00 -0600

    classwork

---
+++ Problem3.js
@@ -0,0 +1,13 @@
+// Problem 3
+// Create a function in your program that counts from 0 to [NUMBER]
+
+number = parseInt(prompt ("Please enter number"))
+
+function countNumbers() {
+
+    for (var i = 0; i <= number; i++) {
+        console.log(i)
+    }
+}
+
+countNumbers()
