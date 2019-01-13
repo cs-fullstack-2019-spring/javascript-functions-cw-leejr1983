74823c99ef9e05b38d758c10fcc486d025b02cb9
leejr1983 <leejr1983@yahoo.com>
2019-01-12 22:36:54 -0600

    classwork

---
+++ Challenge.js
@@ -0,0 +1,31 @@
+// Challenge
+// Create 4 functions called add, subtract, multiply, and divide.
+// Create them to allow a user to perform the name of the function to the two numbers and return the result.
+
+perform = prompt(" Hello. Would you like to add, subtract, multiply, or divide?")
+number1= parseInt(prompt ("Ok. Please enter your 1st number."))
+number2= parseInt(prompt("Please enter your 2nd number so that we may solve this problem."))
+
+function add(){
+    if (perform == "add")
+        return number1 + number2
+
+}
+
+function subtract(){
+    if (perform == "subtract")
+        return number1 - number2
+}
+
+function multiply(){
+    if (perform == "multiply")
+        return number1 * number2
+}
+
+function divide (){
+    if (perform == "divide")
+        return number1 / number2
+}
+
+
+console.log(add() || subtract() || multiply() || divide())
